36bcae0009ae0dfcf28f3b277c5d5dda0086701f
murer <murer@dextra-sw.com>
2014-08-04 13:46:31 -0300

    x

---
+++ configs/murer-ubuntu/openbox/install-openbox.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -xe
+
+sudo apt-get -y install openbox nitrogen tint2 xfce4-power-manager volti xscreensaver gmrun 
+
+if [ ! -f /etc/X11/openbox.original.tar.gz ]; then
+	cd /etc/X11
+	sudo tar czf openbox.original.tar.gz openbox/*
+	sudo rm -rfv openbox/*
+	cd -
+fi
+
+sudo cp -Rv etc/X11/openbox/* /etc/X11/openbox
+cp -v home/xinitrc ~/.xinitrc
+
+
+
