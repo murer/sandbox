f9caadc81a1041502cfec539fc1193d29f004df9
murer <murer@dextra-sw.com>
2014-08-04 18:34:12 -0300

    x

---
+++ configs/murer-ubuntu/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -xe
+
+cd xorg
+./install-xorg.sh
+cd -
+cd wicd
+./install-wicd.sh
+cd -
+cd graphics-util
+./install-graphics-util.sh
+cd -
+cd openbox
+./install-openbox.sh
+cd -
+cd chrome
+./install-chrome.sh
+cd -
+cd slim
+./install-slim.sh
+cd -
+
