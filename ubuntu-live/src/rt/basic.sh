a75f866c0dcfb9b297445e26f026ed3f1c99f187
murer <murer@dextra-sw.com>
2014-08-11 16:03:14 -0300

    x

--- ubuntu-live/src/rt/basic.sh
+++ ubuntu-live/src/rt/basic.sh
@@ -9,4 +9,5 @@ apt-get install -y dbus
 dbus-uuidgen > /var/lib/dbus/machine-id
 dpkg-divert --local --rename --add /sbin/initctl
 
-
+apt-get -y upgrade
+apt-get install -y ubuntu-standard casper lupin-casper discover laptop-detect os-prober linux-generic
