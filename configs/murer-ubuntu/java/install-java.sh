44b0145fabf18001983787927d2e5e5daf59b287
murer <murer@dextra-sw.com>
2014-08-30 13:09:16 -0300

    x

---
+++ configs/murer-ubuntu/java/install-java.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -xe 
+
+cd /opt
+if [ ! -f jdk-8u20-linux-x64.tar.gz ]; then
+	sudo wget https://repoz.dextra.com.br/repoz/r/pub/jdk/oracle/jdk-8u20-linux-x64.tar.gz
+fi
+rm -rf jdk-8u20-linux-x64.tar.gz | cat
+sudo tar xzvf jdk-8u20-linux-x64.tar.gz
+if [ ! -L jdk ]; then
+	sudo ln -s jdk1.8.0_20 jdk;
+else
+	echo 'jdk link already exists. Maybe you want to change it';
+fi;
+
+if ! grep "bash.bashrc.jdk" /etc/bash.bashrc; then
+	sudo tee /etc/bash.bashrc.jdk <<-EOF
+	export JAVA_HOME=/opt/jdk
+	export PATH=\$JAVA_HOME/bin:$PATH
+	EOF
+	echo "# bash.bashrc.jdk" | sudo tee -a /etc/bash.bashrc
+	echo "source /etc/bash.bashrc.jdk" | sudo tee -a /etc/bash.bashrc
+fi
+
+source /etc/bash.bashrc.jdk
+
+
