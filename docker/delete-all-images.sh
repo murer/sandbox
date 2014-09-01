68058a402897bad1026d76e9bd7d273875c0e1e2
murer <murer@dextra-sw.com>
2014-09-01 09:21:37 -0300

    x

--- docker/delete-all-images.sh
+++ docker/delete-all-images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-sudo docker images -a | tail -n +2 | sed "s/\s\+/\t/g" | cut -f3 | xargs sudo docker rmi -f
+sudo docker images -qa | xargs sudo docker rmi -f
 
--- docker/delete-images.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-sudo docker images | tail -n +2 | sed "s/\s\+/\t/g" | cut -f3 | xargs sudo docker rmi -f
-
