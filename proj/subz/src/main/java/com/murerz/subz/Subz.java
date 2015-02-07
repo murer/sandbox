5c903551f05b6f7c6bb786adcdb89ace3879c1c3
murer <murer@dextra-sw.com>
2015-02-07 09:26:27 -0200

    x

---
+++ proj/subz/src/main/java/com/murerz/subz/Subz.java
@@ -0,0 +1,16 @@
+package com.murerz.subz;
+
+public class Subz {
+
+	private String src;
+
+	public Subz src(String src) {
+		this.src = src;
+		return this;
+	}
+
+	public Subz subz() {
+		return this;
+	}
+
+}
---
+++ proj/subz/src/test/java/com/murerz/subz/SubzTest.java
@@ -0,0 +1,12 @@
+package com.murerz.subz;
+
+import org.junit.Test;
+
+public class SubzTest {
+
+	@Test
+	public void testSubz() {
+		new Subz().src("src/test/resources/com/murerz/subz").subz();
+	}
+
+}
