2cb0b69e81f2ec8f676cf2a5db48cc1d9aa4ed6f
murer <murer@dextra-sw.com>
2015-03-25 17:31:40 -0300

    x

--- proj/dsopz/py/exporter.py
+++ proj/dsopz/py/exporter.py
@@ -15,23 +15,29 @@ def load_kinds(dataset, namespace=None):
 	for ent in results['entities']:
 		path = ent['key']['path']
 		name = path[0]['name']
-		if not name.startswith('__'):
+		if not name.startswith('_'):
 			ret.append(name)
 	return ret
 
-def print_iterate(dataset, kinds=[], namespace=None):
+def print_iterate(dataset, kinds=[], namespace=None, keys_only=False):
 	if not kinds:
 		kinds = load_kinds(dataset, namespace)
+	field = '*'
+	if keys_only:
+		field = '__key__'
 	for kind in kinds:
-		reader.print_iterate(dataset, 'select * from ' + kind + ' order by __key__', namespace=namespace, msg=kind)
+		reader.print_iterate(dataset, 
+			'select %s from %s order by __key__' % (field, kind), 
+			namespace=namespace, msg=kind)
 
 def __main():
 	parser = argparse.ArgumentParser(description='Exporter')
 	parser.add_argument('-d', '--dataset', required=True, help='dataset')
-	parser.add_argument('-n', '--namespace', required=True, help='namespace')
+	parser.add_argument('-n', '--namespace', help='namespace')
 	parser.add_argument('-k', '--kinds', nargs='+', help='kinds')
+	parser.add_argument('-o', '--keys-only', help='keys only')
 	args = parser.parse_args()
-	print_iterate(args.dataset, args.kinds, args.namespace)
+	print_iterate(args.dataset, args.kinds, args.namespace, args.keys_only)
 
 if __name__ == '__main__':
 	__main()
