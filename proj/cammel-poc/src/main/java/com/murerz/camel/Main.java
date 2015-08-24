37e9c54887dcb3006d7af91ea8e40f1e9afabbca
murer <paulo.murer@movile.com>
2015-08-24 10:02:25 -0300

    poc

---
+++ proj/cammel-poc/src/main/java/com/murerz/camel/Main.java
@@ -0,0 +1,15 @@
+package com.murerz.camel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+	public static void main(String[] args) {
+		LOG.info("init");
+		
+	}
+
+}
