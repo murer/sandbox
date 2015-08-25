d1ad88b31df9dd8cf06ca36f6c2c8d6ac8f8fac3
paulo-murer-movile <paulo.murer@movile.com>
2015-08-24 23:38:14 -0300

    Update install.sh

--- proj/ts-server/install.sh
+++ proj/ts-server/install.sh
@@ -10,6 +10,8 @@ wget http://dl.4players.de/ts/releases/3.0.11.2/teamspeak3-server_linux-amd64-3.
 tar xzf teamspeak3-server_linux-amd64-3.0.11.2.tar.gz
 mkdir opt
 mv teamspeak3-server_linux-amd64 opt/teamspeak
+sudo ln -s /home/ts/opt/teamspeak/ts3server_startscript.sh /etc/init.d/teamspeak
+sudo update-rc.d teamspeak defaults
 
 cd -
 sudo chown -R ts:ts /home/ts
