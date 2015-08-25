f37edab76c114ba2985718766f234193565357f4
paulo-murer-movile <paulo.murer@movile.com>
2015-08-24 23:29:25 -0300

    Update install.sh

--- proj/ts-server/install.sh
+++ proj/ts-server/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -xe
 
+sudo adduser --disabled-login --gecos ts ts
+
+cd /home/ts
+
 wget http://dl.4players.de/ts/releases/3.0.11.2/teamspeak3-server_linux-amd64-3.0.11.2.tar.gz
 
-
+cd -
