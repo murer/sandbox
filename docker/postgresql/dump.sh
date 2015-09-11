a9354fc45c29b1b188ad46cac10790944860a056
murer <paulo.murer@movile.com>
2015-09-11 11:17:22 -0300

    x

---
+++ docker/postgresql/dump.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -xe
+
+if [ "x$1" == "x" ]; then
+	echo "Usage:";
+	echo "  $0 <host> <port> <user> <password> <database> [-n <schema>, -n <schema>, ...]";
+	exit 1;
+fi
+
+PG_HOST=$1
+PG_PORT=$2
+PG_USER=$3
+PG_PASSWORD=$4
+PG_DATABASE=$5
+shift 5
+
+export PGPASSWORD="$PG_PASSWORD"
+pg_dump -v -O -h $PG_HOST -p $PG_PORT -U $PG_USER $* $PG_DATABASE
