eb818f74eeff03e11e7a9d1fe7b0ae3b634c3bf5
murer <paulo.murer@movile.com>
2015-10-17 21:34:43 -0300

    locales

--- proj/scriptirc/src/protocol.js
+++ proj/scriptirc/src/protocol.js
@@ -4,7 +4,7 @@ function init(client) {
 
     var line = '';
 
-    function onData(evt, data) {
+    function onConnsData(evt, data) {
         line += data;
         var idx = line.indexOf('\n');
         while(idx >= 0) {
@@ -16,11 +16,18 @@ function init(client) {
         }
     }
 
+    function onProtocolData(evt, data) {
+        var array = data.match(/\s*([^\s]+)\s+([^\s]+)\s(.*)$/);
+        var msg = { prefix: array[1], command: array[2], params: array[3] };
+        client.fire('protocol_message', msg);
+    }
+
     function onSend(evt, data) {
         client.fire('conns_send', data += '\r\n');
     }
 
-    client.on('conns_data', onData);
+    client.on('conns_data', onConnsData);
+    client.on('protocol_data', onProtocolData);
     client.on('protocol_send', onSend);
 }
 
