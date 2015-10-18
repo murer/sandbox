4f4d4419c1504b0ea0705994e087e749068425b9
murer <paulo.murer@movile.com>
2015-10-18 01:02:50 -0200

    locales

--- proj/scriptirc/src/nick.js
+++ proj/scriptirc/src/nick.js
@@ -2,13 +2,18 @@ log = require('./log')
 
 function init(client, config) {
 
-    function changeNick(evt, n) {
-        client.fire('protocol_send', 'NICK ' + config.nick);
+    function changeNick(evt, data) {
+        client.fire('protocol_send', 'NICK ' + data);
     } 
 
     function onConnected(evt) {
-    	client.fire('user_nick', config.nick);
+        client.fire('user_nick', config.nick);
+        client.nick = config.nick;
         client.fire('protocol_send', 'USER ' + config.user);
+    }
+
+    function ready(evt) {
+        client.fire('nick_changed', config);
     }
 
     client.on('conns_connected', onConnected);
