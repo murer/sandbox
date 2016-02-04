0d4c72cf5862f25119dd4906e0050d4c6e8a9e7e
murer <murer@dextra-sw.com>
2016-02-04 09:17:53 -0200

    docker

---
+++ proj/httpcachez-node/test/simpleTest.js
@@ -0,0 +1,17 @@
+var testrunner = require('./testrunner');
+
+testrunner.test('test1', function(end) {
+  testrunner.ok(1);
+  end();
+});
+
+testrunner.test('test timeout', function(end) {
+  setTimeout(function() {
+    testrunner.equal(4, 1);
+    end();
+  }, 1);
+});
+
+testrunner.simple('test2', function() {
+  testrunner.ok(1);
+});
--- proj/httpcachez-node/test/test.js
+++ proj/httpcachez-node/test/test.js
@@ -1,23 +1,11 @@
-testrunner = require('./testrunner');
+var testrunner = require('./testrunner');
+
+require('./simpleTest')
+require('./downloaderTest')
 
 //testrunner.onError = function(error) {
 //  throw 'ERROR: ' + this.current.name + ' ' + error.join('');
 //};
 
-testrunner.test('test1', function(end) {
-  testrunner.ok(1);
-  end();
-});
-
-testrunner.test('test timeout', function(end) {
-  setTimeout(function() {
-    testrunner.equal(4, 1);
-    end();
-  }, 1);
-});
-
-testrunner.simple('test2', function() {
-  testrunner.ok(1);
-});
 
 testrunner.execute();
