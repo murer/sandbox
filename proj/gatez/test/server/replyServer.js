6b6ad4febaaa78083140c321c3ddb05720f1d064
murer <murer@dextra-sw.com>
2016-02-08 15:06:17 -0200

    docker

--- proj/gatez/test/server/replyServer.js
+++ proj/gatez/test/server/replyServer.js
@@ -1,26 +1,45 @@
 net = require('net');
 
-exports.server = function(port) {
-  var server = net.createServer(function(socket) {
-    socket.write('Connected\n');
-    var name = socket.remoteAddress + ":" + socket.remotePort
+function Server(port) {
+    var clients = {};
+    var server = net.createServer(function(socket) {
+      var name = socket.remoteAddress + ":" + socket.remotePort
+      clients[name] = socket;
+      socket.write('Connected\n');
 
-    socket.on('data', function(data) {
-      var msg = data.toString('utf-8');
-      var reply = 'Reply: ' + data;
-      socket.write(reply);
-      if(msg.match(/^exit/)) {
-        socket.end();
-      }
+      socket.on('data', function(data) {
+        var msg = data.toString('utf-8');
+        var reply = 'Reply: ' + data;
+        socket.write(reply);
+        if(msg.match(/^exit/)) {
+          socket.end();
+        }
+      });
+      socket.on('end', function() {
+        console.log('Client Disconnected: ' + name);
+        delete(clients[name])
+      });
     });
-    socket.on('end', function() {
-      console.log('Client Disconnected: ' + name);
+    server.listen(port, function() {
+      address = server.address();
+      console.log('Reply server started on %j', address, arguments);
     });
-  });
-  server.listen(port, function() {
-    address = server.address();
-    console.log('Reply server started on %j', address, arguments);
-  });
+
+    this.clients = clients;
+    this.server = server;
 }
 
-exports.server(5000);
+Server.prototype.shutdown = function() {
+  console.log('Shutting down')
+  this.server.close(function() {
+    console.log('Server shutdown')
+  });
+  for(var name in this.clients) {
+    this.clients[name].end('Shutting down\n');
+    delete(this.clients[name]);
+  };
+}
+
+exports.server = function(port) {
+  return new Server(port);
+}
