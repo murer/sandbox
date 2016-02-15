c3edb6f70bd90eb6f2e6cfbea2dfea193aabb451
murer <murer@dextra-sw.com>
2016-02-15 16:07:31 -0200

    docker

--- proj/httpcachez-java/src/main/java/com/murerz/httpcachez/Key.java
+++ proj/httpcachez-java/src/main/java/com/murerz/httpcachez/Key.java
@@ -12,4 +12,38 @@ public class Key {
 		this.path = path;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((host == null) ? 0 : host.hashCode());
+		result = prime * result + ((path == null) ? 0 : path.hashCode());
+		result = prime * result + port;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Key other = (Key) obj;
+		if (host == null) {
+			if (other.host != null)
+				return false;
+		} else if (!host.equals(other.host))
+			return false;
+		if (path == null) {
+			if (other.path != null)
+				return false;
+		} else if (!path.equals(other.path))
+			return false;
+		if (port != other.port)
+			return false;
+		return true;
+	}
+
 }
