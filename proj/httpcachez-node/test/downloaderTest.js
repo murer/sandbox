3162a1231faae06ff730f666f0ded34de3daca3e
murer <murer@dextra-sw.com>
2016-02-15 09:37:41 -0200

    docker

--- proj/httpcachez-node/test/downloaderTest.js
+++ proj/httpcachez-node/test/downloaderTest.js
@@ -5,7 +5,19 @@ testrunner.group('downloaderTest')
 
 testrunner.test('download', function(end) {
   var download = downloader.download('http://repo1.maven.org/maven2/com/googlecode/mycontainer/mycontainer-annotations/1.6.2/mycontainer-annotations-1.6.2.pom.md5')
-  download.done(function() {
-    end();
+  download.on('headers', function(resp) {
+    testrunner.equal(200, resp.code);
+    testrunner.equal('OK', resp.status);
+    testrunner.equal(70, resp.headers['Content-Length']);
   });
+  download.on('data', function(data) {
+    console.log('Received', data.length);
+  });
+  download.on('end', function() {
+    console.log('End');
+  });
+  download.on('error', function(error) {
+    console.log('Error', error);
+  });
+  download.start();
 });
--- proj/httpcachez-node/test/test.js
+++ proj/httpcachez-node/test/test.js
@@ -1,7 +1,8 @@
 var testrunner = require('./testrunner');
 
-require('./simpleTest')
-require('./server/replyServerTest')
+require('./simpleTest');
+require('./server/replyServerTest');
+require('./downloaderTest');
 
 //testrunner.onError = function(error) {
 //  throw 'ERROR: ' + this.current.name + ' ' + error.join('');
