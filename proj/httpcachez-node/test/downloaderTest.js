4d1d97a37f73c2bac4c89f925860c5e5f443369e
murer <murer@dextra-sw.com>
2016-02-15 10:33:36 -0200

    docker

--- proj/httpcachez-node/test/downloaderTest.js
+++ proj/httpcachez-node/test/downloaderTest.js
@@ -3,7 +3,7 @@ var downloader = require('../src/downloader');
 
 testrunner.group('downloaderTest')
 
-testrunner.test('download', 4, function(end) {
+testrunner.test('download http ok', 4, function(end) {
   var download = downloader.download('http://repo1.maven.org/maven2/com/googlecode/mycontainer/mycontainer-annotations/1.6.2/mycontainer-annotations-1.6.2.pom.md5')
   var buffer = new Buffer(0);
   download.on('response', function(resp) {
@@ -16,6 +16,23 @@ testrunner.test('download', 4, function(end) {
   });
   download.on('end', function() {
     testrunner.equal('35021fb13df866d2df394f5d7b169abe', buffer.toString('utf-8'));
+    end();
+  });
+  download.on('error', function(error) {
+    testrunner.fail(error)
+    end();
+  });
+  download.start();
+});
+
+testrunner.test('download http not found', 2, function(end) {
+  var download = downloader.download('http://repo1.maven.org/maven2/com/googlecode/mycontainer/mycontainer-annotations/1.6.2/not-found.txt')
+  var buffer = new Buffer(0);
+  download.on('response', function(resp) {
+    testrunner.equal(404, resp.code);
+    testrunner.equal('Not Found', resp.message);
+  });
+  download.on('end', function() {
     end();
   });
   download.on('error', function(error) {
