09822bc0b1ae8d57e0c28bc5a46be1d1754339a9
murer <murer@dextra-sw.com>
2016-02-15 16:37:39 -0200

    docker

--- proj/httpcachez-java/src/main/java/com/murerz/httpcachez/HTTP.java
+++ proj/httpcachez-java/src/main/java/com/murerz/httpcachez/HTTP.java
@@ -4,10 +4,8 @@ import java.io.IOException;
 
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpHost;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.util.EntityUtils;
 
