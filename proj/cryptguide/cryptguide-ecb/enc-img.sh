a4d1d0432ff2df70d1ae6be53ea079d76beb087f
murer <murer@dextra-sw.com>
2016-12-20 16:46:45 -0200

    init

---
+++ proj/cryptguide/cryptguide-ecb/enc-img.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -xe
+
+mkdir target || true
+
+key='7d61bb87ebe736b23e9985f76481a785'
+iv='00112233445566778899AABBCCDDEEFF'
+
+(head -c '1001' img/test.bmp && \
+   (tail -c '+1001' img/test.bmp | openssl enc -aes-128-ecb -K "$key" -nosalt)) > target/test.ecb.bmp
+
+(head -c '1001' img/test.bmp && \
+  (tail -c '+1001' img/test.bmp | openssl enc -aes-128-cbc -K "$key" -nosalt -iv "$iv")) > target/test.cbc.bmp
