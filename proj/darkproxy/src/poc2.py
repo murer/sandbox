d53809777b1cb4fa01543b13b080b14764411bf3
murer <murer@dextra-sw.com>
2016-12-30 13:57:58 -0200

    x

---
+++ proj/darkproxy/src/poc2.py
@@ -0,0 +1,70 @@
+import SocketServer
+import BaseHTTPServer
+import SimpleHTTPServer
+
+class ThreadingSimpleServer(SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer):
+
+    def req(self, req):
+        self.server.req(req)
+
+class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+
+    """
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def req(self, method):
+        self._set_headers()
+        self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+    """
+
+    def req(self, method):
+        self.server.req(self)
+
+    def do_GET(self):
+        self.req('GET')
+    def do_HEAD(self):
+        self.req('HEAD')
+    def do_PUT(self):
+        self.req('PUT')
+    def do_POST(self):
+        self.req('POST')
+    def do_DELETE(self):
+        self.req('DELETE')
+
+
+class Server(object):
+
+    def __init__(self, handler, port=8000):
+        self.port = port
+        self.handler = handler
+
+    def serve(self):
+        self.simple_server = ThreadingSimpleServer(('', self.port), RequestHandler)
+        self.simple_server.server = self
+        self.simple_server.serve_forever()
+
+    def req(self, req):
+        self.handler.req(req)
+
+class Handler(object):
+
+    def req(self, req):
+        """ implement it """
+
+class SimpleHandler(object):
+
+    def req(self, req):
+        req.send_response(200)
+        req.send_header('Content-type', 'text/plain; charset=UTF-8')
+        req.end_headers()
+        req.wfile.write("OK")
+
+def _main():
+    proxy = Server(SimpleHandler(), port=8000)
+    proxy.serve()
+
+if __name__ == "__main__":
+    _main()
