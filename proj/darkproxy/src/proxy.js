cd29d2cbcba0c6cbd2e032619b8873f036db3e19
murer <murer@dextra-sw.com>
2017-01-04 00:57:48 -0200

    'x'

--- proj/darkproxy/src/proxy.js
+++ proj/darkproxy/src/proxy.js
@@ -21,7 +21,7 @@ function proxy(self, msg, commandReq, commandResp) {
         darkutil.loadBody(resp, commandResp, (body) => {
             msg.data.resp.body = body;
             console.log('proxy resp', msg.data.resp);
-            darkutil.sendJson(commandResp, 'OK');
+            darkutil.sendJson(commandResp, msg.data.resp);
         });
     });
     req.on('error', (err) => {
