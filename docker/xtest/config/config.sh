3e0589bdcafcceff25ee9b36fcb0b66bd8bf8b51
murer <murer@dextra-sw.com>
2017-05-16 16:03:31 -0300

    fix

--- docker/xtest/config/config.sh
+++ docker/xtest/config/config.sh
@@ -2,5 +2,6 @@
 
 Xvfb :99 -screen 0 1024x768x24 -ac &
 sleep 2
-openbox --startup /etc/X11/openbox/autostart &
+#openbox --startup /etc/X11/openbox/autostart &
+openbox-session &
 x11vnc -display :99 -forever -nopw
