8e9a035b6f5afaa67a924b47fae414a17d78a123
murer <murer@dextra-sw.com>
2018-01-21 18:42:30 -0200

    test

--- poc/ubuntu-live/src/build/helper/chroot_customize.sh
+++ poc/ubuntu-live/src/build/helper/chroot_customize.sh
@@ -4,16 +4,4 @@ export DEBIAN_FRONTEND=noninteractive
 export HOME=/root
 export LC_ALL=C
 
-mount none -t proc /proc
-mount none -t sysfs /sys
-mount none -t devpts /dev/pts
-
-apt-get -y update
-apt-get install --yes dbus
-dbus-uuidgen > /var/lib/dbus/machine-id
-#dpkg-divert --local --rename --add /sbin/initctl
-
-#apt-get -y upgrade
-apt-get install -y ubuntu-standard casper lupin-casper
-apt-get install -y discover laptop-detect os-prober
-apt-get install -y linux-generic
+echo 'Customize'
