7aec7e9460366887fe0de968b6be4f3415048cee
murer <murer@dextra-sw.com>
2018-01-21 18:42:58 -0200

    test

---
+++ poc/ubuntu-live/src/build/helper/chroot_cleanup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -xe
+
+export DEBIAN_FRONTEND=noninteractive
+export HOME=/root
+export LC_ALL=C
+
+rm /var/lib/dbus/machine-id
+apt-get clean
+rm -rf /tmp/*
+umount -lf /proc
+umount -lf /sys
+umount -lf /dev/pts
