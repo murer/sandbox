b6cf32ff135061ffbcc4f94f592cdd8d7dbe6496
murer <murer@dextra-sw.com>
2018-01-21 21:21:48 -0200

    test

--- configs/murer-ubuntu/basics/install-basics.sh
+++ configs/murer-ubuntu/basics/install-basics.sh
@@ -1,4 +1,3 @@
 #!/bin/bash -xe
 
-sudo apt-get install git subversion nmap netcat vim curl wget pv zip connect-proxy tcpdump zip pv bc
-
+sudo apt-get -y install git subversion nmap netcat vim curl wget pv zip connect-proxy tcpdump zip pv bc
