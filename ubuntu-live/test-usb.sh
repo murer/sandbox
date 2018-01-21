2698238da056c7ece55a848f583a0e273be5210f
murer <murer@dextra-sw.com>
2018-01-21 19:46:01 -0200

    test

--- ubuntu-live/test-usb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -xe
-
-scp -rP 6022 src ubuntu@localhost:
-ssh -p 6022 ubuntu@localhost src/create-usb.sh "$1"
-
--- ubuntu-live/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xe
-
-scp -rP 6022 src ubuntu@localhost:
-ssh -p 6022 ubuntu@localhost chmod +x src/create-iso.sh
-ssh -p 6022 ubuntu@localhost src/chroot-init.sh
-ssh -p 6022 ubuntu@localhost src/chroot-clean.sh
-ssh -p 6022 ubuntu@localhost src/create-iso.sh
-
