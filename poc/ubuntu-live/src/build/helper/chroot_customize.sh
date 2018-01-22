c45a4a8e9a3f5cb31da3ee304be7eb4ded546bdc
murer <murer@dextra-sw.com>
2018-01-21 23:05:51 -0200

    test

--- poc/ubuntu-live/src/build/helper/chroot_customize.sh
+++ poc/ubuntu-live/src/build/helper/chroot_customize.sh
@@ -4,26 +4,20 @@ export DEBIAN_FRONTEND=noninteractive
 export HOME=/root
 export LC_ALL=C
 
-apt-get install -y git
+sudo apt-get -y \
+  install git subversion nmap netcat vim curl wget \
+  pv zip connect-proxy tcpdump zip pv bc \
+  wicd wicd-curses wicd-gtk \
+  firefox gedit gmrun lxterminal gedit gnome-screenshot pcmanfm \
+  openbox tint2 xfce4-power-manager xscreensaver arandr
 
-[[ -d /livebuild/sandbox ]] || git clone https://github.com/murer/sandbox /livebuild/sandbox
-
-cd /livebuild/sandbox/configs/murer-ubuntu
-cd basics
-./install-basics.sh
-cd ..
-cd xorg
-./install-xorg.sh
-cd ..
-cd wicd
-./install-wicd.sh
-cd ..
-cd graphics-util
-./install-graphics-util.sh
-cd ..
-cd openbox
-./install-openbox.sh
-cd /
+if [ ! -f /etc/X11/openbox.original.tar.gz ]; then
+	cd /etc/X11
+	sudo tar czf openbox.original.tar.gz openbox/*
+	sudo rm -rfv openbox/*
+	cd -
+fi
+sudo cp -Rv /livebuild/conf/openbox/* /etc/X11/openbox
 
 wget --no-check-certificate -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
