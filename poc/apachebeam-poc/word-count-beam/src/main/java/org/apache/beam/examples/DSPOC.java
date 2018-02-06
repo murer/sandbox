fc18814b520ef05d4fa77762daa9a96f2ea8cf72
murer <murer@dextra-sw.com>
2018-02-06 15:43:19 -0200

    test

--- poc/apachebeam-poc/word-count-beam/src/main/java/org/apache/beam/examples/DSPOC.java
+++ poc/apachebeam-poc/word-count-beam/src/main/java/org/apache/beam/examples/DSPOC.java
@@ -1,10 +1,18 @@
 package org.apache.beam.examples;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.Combine;
+import org.apache.beam.sdk.transforms.Combine.CombineFn;
+import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PDone;
 import org.apache.beam.sdk.values.PInput;
@@ -27,10 +35,47 @@ public class DSPOC {
 		PipelineOptions options = PipelineOptionsFactory.create();
 		Pipeline p = Pipeline.create(options);
 
-		PCollection<String> c1 = p.apply(TextIO.read().from("pom.xml"));
-		PCollection<String> c2 = (PCollection<String>) c1.apply(noop());
-		System.out.println("c1 == c2: " + (c1 == c2));
-		PDone done = c2.apply(TextIO.write().to("target/poc/output"));
+		PCollection<String> c = p.apply(TextIO.read().from("pom.xml"));
+		c = c.apply(ParDo.of(new DoFn<String, String>() {
+			@ProcessElement
+			public void processElement(ProcessContext c) {
+				String[] array = c.element().split("[^A-Za-z0-9]+");
+				for (String word : array) {
+					c.output(word);
+				}
+			}
+		}));
+		c = c.apply(Combine.globally(new CombineFn<String, Map<String, Long>, String>() {
+
+			public Map<String, Long> createAccumulator() {
+				return new HashMap<String, Long>();
+			}
+
+			@Override
+			public Map<String, Long> addInput(Map<String, Long> accumulator, String input) {
+				Long num = accumulator.get(input);
+				accumulator.put(input, num == null ? 1L : num + 1L);
+				return accumulator;
+			}
+
+			@Override
+			public Map<String, Long> mergeAccumulators(Iterable<Map<String, Long>> accumulators) {
+				Map<String, Long> ret = new HashMap<String, Long>();
+				for (Map<String, Long> accumulator : accumulators) {
+					for (Entry<String, Long> entry : accumulator.entrySet()) {
+						Long num = ret.get(entry.getKey());
+						ret.put(entry.getKey(), num == null ? entry.getValue() : num + entry.getValue());
+					}
+				}
+				return ret;
+			}
+
+			@Override
+			public String extractOutput(Map<String, Long> accumulator) {
+				return accumulator.toString();
+			}
+		}));
+		PDone done = c.apply(TextIO.write().to("target/poc/output"));
 
 		p.run().waitUntilFinish();
 	}
