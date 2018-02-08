90ad9789840ec5597c7437dfb6fe27253a2cb94f
murer <murer@dextra-sw.com>
2018-02-08 09:15:00 -0200

    test

--- poc/apachebeam-poc/word-count-beam/cmds/dataproc-spark.sh
+++ poc/apachebeam-poc/word-count-beam/cmds/dataproc-spark.sh
@@ -2,6 +2,19 @@
 
 mvn package -Pspark-runner
 
-gsutil cp \
-  /home/murer/proj/sandbox/poc/apachebeam-poc/word-count-beam/target/word-count-beam-bundled-0.1.jar \
-  gs://mydataflow-poc/sample/test.jar
+gsutil -m rm -r gs://mydataflow-poc/sample gs://mydataflow-poc/result gs://mydataflow-poc/tmp || true
+gsutil -m cp -r sample gs://mydataflow-poc
+
+#gsutil cp \
+#  /home/murer/proj/sandbox/poc/apachebeam-poc/word-count-beam/target/word-count-beam-bundled-0.1.jar \
+#  gs://mydataflow-poc/sample/test.jar
+
+gcloud dataproc jobs submit spark \
+  --region us-east1 \
+  --cluster mycluster \
+  --jars=target/word-count-beam-bundled-0.1.jar \
+  --class=org.apache.beam.examples.DSPOC \
+  -- \
+  --runner=SparkRunner \
+  --inputFile=gs://mydataflow-poc/sample/input.csv \
+  --output=gs://mydataflow-poc/result/ret
--- poc/apachebeam-poc/word-count-beam/cmds/mvn-ds-spark-sa.sh
+++ poc/apachebeam-poc/word-count-beam/cmds/mvn-ds-spark-sa.sh
@@ -7,7 +7,5 @@ mvn compile exec:java \
   -Pspark-runner \
   -Dexec.mainClass=org.apache.beam.examples.DSPOC \
   -Dexec.args="--runner=SparkRunner \
-    --project=frotanetappdevel \
-    --gcpTempLocation=gs://mydataflow-poc/tmp \
-    --inputFile=gs://mydataflow-poc/sample/input.csv \
-    --output=gs://mydataflow-poc/result/ret"
+  --inputFile=sample/input.simple.csv \
+  --output=target/poc/result"
