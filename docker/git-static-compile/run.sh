030766b2befa5900f28cd9096da9b4ae43dcf104
murer <murer@dextra-sw.com>
2018-04-23 16:33:07 -0300

    x

--- docker/git-static-compile/run.sh
+++ docker/git-static-compile/run.sh
@@ -5,5 +5,5 @@ mkdir target
 
 docker rm -f git-static-compile || true
 docker run -it --name git-static-compile git-static-compile /bin/true
-docker cp git-static-compile:/root/git.tar.gz target/git.tar.gz
+docker cp git-static-compile:/home/git/git-static.tar.gz target/git-static.tar.gz
 docker rm -f git-static-compile || true
