688f6983453bf1ad7cf87a22cf148ccd8d852f69
murer <murer@dextra-sw.com>
2018-05-03 12:48:08 -0300

    x

---
+++ proj/darkproxy/src/TcpConn.js
@@ -0,0 +1,149 @@
+const net = require('net');
+const util = require('util');
+
+class TcpConn {
+  static connect(host, port) {
+    return new Promise((resolve, reject) => {
+      let ret = new TcpConn();
+      ret.buffer = Buffer.from([]);
+      ret.state = 'CREATED';
+      ret.maxBufferSize = 16*1024;
+      ret.socket = net.connect(port, host, () => {
+        ret.state = 'CONNECTED';
+        //ret.socket.resume();
+        resolve(ret);
+      });
+      console.log('readableHighWaterMark', ret.socket.readableHighWaterMark)
+
+      ret.socket.on('error', (err) => {
+        if(ret.state === 'CREATED') {
+          return reject(err);
+        }
+        console.log('error', err);
+      });
+
+      ret.socket.on('end', () => {
+        console.log('connection end');
+      });
+
+      ret.socket.on('close', (hasError) => {
+        console.log('connection close', hasError);
+        ret.state = 'CLOSED';
+      });
+
+      //ret.socket.pause();
+
+      /*
+      ret.socket.on('data', (data) => {
+        console.log('internal data', data.length, ret.buffer.length + data.length);
+        ret.buffer = Buffer.concat([ret.buffer, data]);
+        if(ret.buffer.length >= ret.maxBufferSize) {
+          console.log('pause')
+          ret.socket.pause();
+        }
+      });
+      */
+
+    });
+  }
+
+  async write(data) {
+    return new Promise((resolve, reject) => {
+      console.log('write', data);
+      this.socket.write(data);
+      setTimeout(() => {
+        resolve();
+      }, 500);
+    });
+  }
+
+  async read(max) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if(max === 0) {
+          return resolve(Buffer.from([]));
+        }
+        let data = this.socket.read();
+        console.log('internal read', max, data, this.state);
+        if(!data && this.state === 'CLOSED') {
+          console.log('reading closed socket');
+          return null;
+        }
+        console.log('internal data', data.length, max);
+        if(data.length < max) {
+          return resolve(data);
+        }
+        let ret = data.slice(0, max);
+        let remaining = data.slice(max, data.length);
+        console.log('unshift', remaining.length);
+        this.socket.unshift(remaining);
+        resolve(ret);
+      }, 500);
+      /*
+      console.log('check resume', max, this.buffer.length, this.socket.isPaused());
+      if(max > this.buffer.length) {
+        console.log('resume');
+        this.socket.resume();
+      }
+      max = Math.min(this.buffer.length, max);
+      console.log('bufferSize', this.socket.bufferSize, this.buffer.length);
+      if(max === 0 && !this.socket.bufferSize && this.state === 'CLOSED') {
+        resolve(null);
+        return;
+      }
+      if(max === 0) {
+        setTimeout(() => {
+          resolve(Buffer.from([]));
+        }, 500);
+        return;
+      }
+      let ret = this.buffer.slice(0, max);
+      this.buffer = this.buffer.slice(max, this.buffer.length);
+      console.log('reading', max, ret.length, this.buffer.length);
+      resolve(ret);
+      */
+    });
+  }
+
+  toString() {
+    return `${this.socket.localAddress}:${this.socket.localPort}-${this.socket.remoteAddress}:${this.socket.remotePort}`;
+  }
+}
+
+async function main(args) {
+
+  async function sleep(time) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve();
+      }, time);
+    });
+  }
+
+  process.on('unhandledRejection', (reason, p) => {
+    console.log('FAIL', reason);
+    process.exit(1);
+  });
+
+  console.log('args', args);
+
+  let conn = await TcpConn.connect(args[2], args[3]);
+  console.log(`connected ${conn}`);
+  //await conn.write('01234');
+  //await conn.write('56789');
+  while(true) {
+    let data = await conn.read(10000);
+    if(!data) {
+      break;
+    }
+    console.log('read', data.length, conn.socket.bufferLength);
+    await sleep(1000);
+  }
+  console.log('done');
+}
+
+if (require.main === module) {
+    main(process.argv);
+}
+
+exports.TcpConn = TcpConn;
