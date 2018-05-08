825a7bccca34f84b62e188143531866a5328878b
murer <murer@dextra-sw.com>
2018-05-08 15:19:37 -0300

    x

--- proj/darkproxy/src/http/HttpProxy.js
+++ proj/darkproxy/src/http/HttpProxy.js
@@ -21,39 +21,43 @@ class HttpProxy {
       ret.server.on('error', reject);
       ret.server.on('request', async (req, resp) => {
         let conn = new HttpProxyConnection(new hc.HttpRequest(req), new hc.HttpResponse(resp));
-        ret.onRequest(conn);
+        await ret.onRequest(conn);
+        console.log('DONE')
       });
     });
   }
 
-  async onRequest(conn) {
-    let req = conn.req;
-    let resp = conn.resp;
-    console.log(`HttpProxy in request: ${req}`);
-    let target = req.req.headers.host.split(':');
-    let opts = {
-      hostname: target[0],
-      port: target[1] || '80',
-      method: req.req.method,
-      path: req.req.url
-    }
+  onRequest(conn) {
+    return new Promise(async (resolve, reject) => {
+      let req = conn.req;
+      let resp = conn.resp;
+      console.log(`HttpProxy in request: ${req}`);
+      let target = req.req.headers.host.split(':');
+      let opts = {
+        hostname: target[0],
+        port: target[1] || '80',
+        method: req.req.method,
+        path: req.req.url
+      }
 
-    let clientReq = hc.HttpClient.request(opts);
-    console.log(`HttpProxy out request: ${clientReq}`);
-    let clientResp = await clientReq.end()
-    console.log(`HttpProxy in reponse: ${clientResp}`);
+      let clientReq = hc.HttpClient.request(opts);
+      console.log(`HttpProxy out request: ${clientReq}`);
+      let clientResp = await clientReq.end()
+      console.log(`HttpProxy in reponse: ${clientResp}`);
 
-    resp.statusCode = clientResp.resp.statusCode;
-    resp.statusMessage = clientResp.resp.statusMessage;
-    for(let i = 0; i < clientResp.resp.rawHeaders.length; i+=2) {
-      let name = clientResp.resp.rawHeaders[i];
-      let value = clientResp.resp.rawHeaders[i+1];
-      let array = resp.resp.getHeader(name) || [];
-      array.push(value);
-      resp.resp.setHeader(name, array);
-    }
-    console.log(`HttpProxy out response: ${resp}`);
-    clientResp.resp.pipe(resp.resp);
+      resp.statusCode = clientResp.resp.statusCode;
+      resp.statusMessage = clientResp.resp.statusMessage;
+      for(let i = 0; i < clientResp.resp.rawHeaders.length; i+=2) {
+        let name = clientResp.resp.rawHeaders[i];
+        let value = clientResp.resp.rawHeaders[i+1];
+        let array = resp.resp.getHeader(name) || [];
+        array.push(value);
+        resp.resp.setHeader(name, array);
+      }
+      console.log(`HttpProxy out response: ${resp}`);
+      clientResp.resp.pipe(resp.resp);
+      resp.resp.once('finish', resolve)
+    });
   }
 
 }
