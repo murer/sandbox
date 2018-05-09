d0e2e8503ff8b538f44d34ea49e3a6b84ff50d67
murer <murer@dextra-sw.com>
2018-05-09 16:09:01 -0300

    x

--- proj/darkproxy/src/Logger.js
+++ proj/darkproxy/src/Logger.js
@@ -12,7 +12,7 @@ class Logger {
   }
 
   log(level, args) {
-    target[level.toLowerCase()](`${new Date().toISOString()} [${level}] ${this.simpleName}`, ...args);
+    target[level.toLowerCase()](`${new Date().toISOString()} ${this.simpleName} [${level}]`, ...args);
   }
 
   debug(...args) { return this.log('DEBUG', args); }
