56e6b7ce9720c4fae840354b873441ddbc5b163c
murer <murer@dextra-sw.com>
2018-08-21 15:41:51 -0300

    x

--- configs/murer-ubuntu/i3/install-i3.sh
+++ configs/murer-ubuntu/i3/install-i3.sh
@@ -5,10 +5,11 @@ sudo apt-get -y install i3 gmrun arandr pcmanfm
 if [ ! -f /etc/i3.original.tar.gz ]; then
 	cd /etc
 	sudo tar czf i3.original.tar.gz i3
+	sudo rm -rfv i3
 	cd -
 fi
 
-sudo cp -Rv etc/i3/* /etc/i3
+sudo cp -Rv etc/i3 /etc/i3
 
 cp -v home/xinitrc ~/.xinitrc
 cp -v home/xinitrc ~/.xsessionrc
