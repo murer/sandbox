d2d2906afb466897272797eed7a24c9f776e1947
murer <murer@dextra-sw.com>
2019-09-27 13:21:58 -0300

    x

--- poc/couchdb-poc/docker/docker-entrypoint.sh
+++ poc/couchdb-poc/docker/docker-entrypoint.sh
@@ -15,25 +15,25 @@ echo 'admin = 123' >> /opt/couchdb/etc/local.d/admins.ini
 
 /docker-entrypoint-original.sh /opt/couchdb/bin/couchdb
 
-sleep 2
-
-curl -vX PUT 'http://localhost:5984/_node/_local/_config/admins/admin' -d '"123"'
-
-
-curl -vX POST -H "Content-Type: application/json" \
-  'http://admin:123@127.0.0.1:5984/_cluster_setup' \
-  -d '{
-    "action": "enable_cluster",
-    "bind_address":"0.0.0.0",
-    "username": "admin",
-    "password":"password",
-    "node_count":"3"
-  }'
-
-curl http://admin:123@172.25.0.2:5984/_cluster_setup
-
-curl -X POST -H "Content-Type: application/json" \
-  http://admin:123@127.0.0.1:5984/_cluster_setup \
-  -d '{"action": "enable_cluster", "bind_address":"0.0.0.0", "username": "admin", "password":"password", "node_count":"2"}'
-
-fg
+# sleep 2
+#
+# curl -vX PUT 'http://localhost:5984/_node/_local/_config/admins/admin' -d '"123"'
+#
+#
+# curl -vX POST -H "Content-Type: application/json" \
+#   'http://admin:123@127.0.0.1:5984/_cluster_setup' \
+#   -d '{
+#     "action": "enable_cluster",
+#     "bind_address":"0.0.0.0",
+#     "username": "admin",
+#     "password":"password",
+#     "node_count":"3"
+#   }'
+#
+# curl http://admin:123@172.25.0.2:5984/_cluster_setup
+#
+# curl -X POST -H "Content-Type: application/json" \
+#   http://admin:123@127.0.0.1:5984/_cluster_setup \
+#   -d '{"action": "enable_cluster", "bind_address":"0.0.0.0", "username": "admin", "password":"password", "node_count":"2"}'
+#
+# fg
