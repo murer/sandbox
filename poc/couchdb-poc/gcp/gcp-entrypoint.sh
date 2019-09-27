ea7866481777bc531967fecd39ccaa1e6c1e95cc
murer <murer@dextra-sw.com>
2019-09-27 18:23:27 -0300

    x

--- poc/couchdb-poc/gcp/gcp-entrypoint.sh
+++ poc/couchdb-poc/gcp/gcp-entrypoint.sh
@@ -25,7 +25,7 @@ apt-get -y update
 apt-get -y install docker-ce
 
 curl https://sdk.cloud.google.com | \
-    CLOUDSDK_CORE_DISABLE_PROMPTS=1 CLOUDSDK_INSTALL_DIR="$/root/opt" bash > /dev/null
+    CLOUDSDK_CORE_DISABLE_PROMPTS=1 CLOUDSDK_INSTALL_DIR="/root/opt" bash > /dev/null
 export PATH="/root/opt/google-cloud-sdk/bin:$PATH"
 /root/opt/google-cloud-sdk/bin/gcloud config set disable_usage_reporting true
 /root/opt/google-cloud-sdk/bin/gcloud components install docker-credential-gcr -q
