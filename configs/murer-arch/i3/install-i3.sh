b38e49f5b32cbd3d899250555f711f761a3b37e6
murer <murer@dextra-sw.com>
2020-01-02 16:29:27 -0400

    x

---
+++ configs/murer-arch/i3/install-i3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -xe
+
+basedir="$(dirname "$0")"
+cd "$basedir"
+
+sudo pacman --noconfirm -S i3-wm i3lock i3status
+
+# if [ ! -f /etc/i3.original.tar.gz ]; then
+# 	cd /etc
+# 	sudo tar czf i3.original.tar.gz i3
+# 	sudo rm -rfv i3
+# 	cd -
+# fi
+#
+# sudo cp -TRv etc/i3 /etc/i3
+#
+# cp -v home/xinitrc ~/.xinitrc
+# cp -v home/xinitrc ~/.xsessionrc
