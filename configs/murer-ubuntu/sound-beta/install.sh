a7742d5f464f2ca890b53d082edecb22c152d8fe
murer <murer@dextra-sw.com>
2020-01-10 00:05:37 +0000

    x

--- configs/murer-ubuntu/sound-beta/install.sh
+++ configs/murer-ubuntu/sound-beta/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -xe
 
-apt-get install alsa-base alsa-utils alsa-oss gstreamer1.0-alsa libasound2 libasound2-plugins
 
+#apt-get install alsa-base alsa-utils alsa-oss gstreamer1.0-alsa libasound2 libasound2-plugins
+
+apt-get install pavucontrol # reboot required ?! :(
 
