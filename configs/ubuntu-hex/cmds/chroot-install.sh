451cb82b6572708cf3a274b50e22d4812ea55444
murer <murer@dextra-sw.com>
2020-01-20 09:54:54 -0300

    x

--- configs/ubuntu-hex/cmds/chroot-install.sh
+++ configs/ubuntu-hex/cmds/chroot-install.sh
@@ -10,6 +10,12 @@ cp -R target/config/etc.pre/* /mnt/installer/etc
 arch-chroot /mnt/installer ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 arch-chroot /mnt/installer locale-gen en_US.UTF-8
 arch-chroot /mnt/installer dpkg-reconfigure -f non-interactive tzdata
+
+[[ -d "/mnt/installer/home/$(cat target/config/user/user.txt)" ]] || \
+  sudo arch-chroot /mnt/installer useradd -u 1000 -g 1000 -m -G adm,cdrom,sudo,dip,plugdev,lpadmin,sambashare -s /bin/bash "$(cat target/config/user/user.txt)" -p "$(cat target/config/user/pass.txt)" || true
+
+false
+
 arch-chroot /mnt/installer apt-get -y update
 arch-chroot /mnt/installer apt-get -y install ubuntu-standard language-pack-en-base software-properties-common
 arch-chroot /mnt/installer apt-get -y install network-manager
