0395b3d2841a587b9400b1d71de53d8a4d3874ae
murer <murer@dextra-sw.com>
2020-02-28 11:36:19 -0300

    x

---
+++ poc/hashicorp-vault/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -xe
+
+cmd_image() {
+  docker build -t murer/hashcorp-vault:latest .
+}
+
+cmd_run() {
+  docker rm -f pyrnet || true
+  docker run -it --rm --name pyrnet \
+    --privileged \
+    --network host \
+    --volume /var/run/dbus:/var/run/dbus \
+    murer/pyrnet "$@"
+}
+
+cmd_start() {
+  #cmd_run mitmproxy  --showhost --mode transparent
+  cmd_run
+}
+
+cmd_shell() {
+  docker exec -it pyrnet /bin/bash
+}
+
+cmd_local() {
+  ~/.local/bin/mitmproxy --mode transparent --showhost --set "confdir=$(pwd)/conf"
+}
+
+cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"
