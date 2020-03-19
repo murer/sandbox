442c3b7648cc4d1c27d99dcb8c780ee3015c96b7
murer <murer@dextra-sw.com>
2020-03-19 15:10:57 -0300

    x

--- proj/vpn-gcp/build.sh
+++ proj/vpn-gcp/build.sh
@@ -3,6 +3,8 @@
 cmd_network_delete() {
   gcloud compute firewall-rules delete neta-allow-basic -q &
   gcloud compute firewall-rules delete netb-allow-basic -q &
+  gcloud beta compute networks subnets delete neta-suba --project dsavault -q --region us-east1 &
+  gcloud beta compute networks subnets delete neta-subb --project dsavault -q --region us-east1 &
   wait
   gcloud compute networks delete neta --project dsavault -q &
   gcloud compute networks delete netb --project dsavault -q &
@@ -10,32 +12,55 @@ cmd_network_delete() {
 }
 
 cmd_network_create() {
-  gcloud compute networks create neta --project dsavault --subnet-mode auto &
+  gcloud compute networks create neta --project dsavault --subnet-mode custom &
   gcloud compute networks create netb --project dsavault --subnet-mode auto &
   wait
+  gcloud beta compute networks subnets create neta-suba --project dsavault \
+    --network neta --region us-east1 \
+    --range=10.0.20.0/24 --enable-private-ip-google-access &
+  gcloud beta compute networks subnets create neta-subb --project dsavault \
+    --network neta --region us-east1 \
+    --range=10.0.21.0/24 --enable-private-ip-google-access &
   gcloud compute firewall-rules create neta-allow-basic --network neta --allow tcp:22,tcp:3389,icmp &
   gcloud compute firewall-rules create netb-allow-basic --network netb --allow tcp:22,tcp:3389,icmp &
   wait
 }
 
 cmd_instance_delete() {
-  gcloud compute instances delete myvpn-u1 --project dsavault --zone us-east1-b -q &
-  gcloud compute instances delete myvpn-u2 --project dsavault --zone us-east1-b -q &
-  wait %1 && wait %2
+  gcloud compute instances delete ivpn-neta-suba-1 --project dsavault --zone us-east1-b -q &
+  gcloud compute instances delete ivpn-neta-suba-2 --project dsavault --zone us-east1-b -q &
+  gcloud compute instances delete ivpn-neta-subb-1 --project dsavault --zone us-east1-b -q &
+  gcloud compute instances delete ivpn-neta-subb-2 --project dsavault --zone us-east1-b -q &
+  gcloud compute instances delete ivpn-netb-def-1 --project dsavault --zone us-east1-b -q &
+  gcloud compute instances delete ivpn-netb-def-2 --project dsavault --zone us-east1-b -q &
+  wait
 }
 
-cmd_instance_create() {
-    # --image-project=ubuntu-os-cloud \
-    # --image-family=ubuntu-1804-lts \
-    # --image-project=debian-cloud \
-    # --image-family=debian-10 \
+instance_create() {
+  # --image-project=ubuntu-os-cloud \
+  # --image-family=ubuntu-1804-lts \
+  # --image-project=debian-cloud \
+  # --image-family=debian-10 \
 
-  gcloud compute instances create myvpn-u1 myvpn-u2 \
+  inst_name="${1?'instance name'}"
+  shift
+  gcloud compute instances create "$inst_name" \
     --project dsavault --zone us-east1-b \
     --machine-type f1-micro \
     --image-project=ubuntu-os-cloud \
     --image-family=ubuntu-1804-lts \
-    --can-ip-forward
+    --can-ip-forward \
+    "$@"
+}
+
+cmd_instance_create() {
+  instance_create ivpn-neta-suba-1 --network-interface network=neta,subnet=neta-suba &
+  instance_create ivpn-neta-suba-2 --network-interface network=neta,subnet=neta-suba &
+  instance_create ivpn-neta-subb-1 --network-interface network=neta,subnet=neta-subb &
+  instance_create ivpn-neta-subb-2 --network-interface network=neta,subnet=neta-subb &
+  instance_create ivpn-netb-def-1 --network-interface network=netb &
+  instance_create ivpn-netb-def-2 --network-interface network=netb &
+  wait
 }
 
 cmd_recreate() {
