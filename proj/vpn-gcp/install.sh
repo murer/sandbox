e8e0c47abc396e030b4ed1fab900a908b39fbf35
murer <murer@dextra-sw.com>
2020-03-19 17:31:27 -0300

    x

--- proj/vpn-gcp/install.sh
+++ proj/vpn-gcp/install.sh
@@ -3,8 +3,14 @@
 # openssl rand 66 | base64 -w0
 vpn_ipsec_secret='WfXTHm026amU1fVwnXdmiTw/405NtDiqVFNiNM+RMmNRvIMK/+3uRBBful0ImtdWNid4j9cr176mAOGy7K3mxyun'
 
-vpn_instance_u1="$(dig +short myvpn-u1.c.dsavault.internal)"
-vpn_instance_u2="$(dig +short myvpn-u2.c.dsavault.internal)"
+vpn_ipsec_me=10.0.20.209
+vpn_ipsec_other=10.0.20.208
+if hostname -I | grep 10.0.20.208; then
+  vpn_ipsec_me=10.0.20.208
+  vpn_ipsec_other=10.0.20.209
+fi
+
+echo "ipsec $vpn_ipsec_me -> $vpn_ipsec_other"
 
 cp /etc/sysctl.conf /etc/sysctl.conf.bak
 sed -i -r 's/#?(net.ipv4.ip_forward)\s*=.*/\1=1/g' /etc/sysctl.conf
