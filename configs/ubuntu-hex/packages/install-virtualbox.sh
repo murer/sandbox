1f178914f9a9438a507b9c2aea13457245ec6156
murer <murer@dextra-sw.com>
2020-08-15 19:30:50 -0300

    init

--- configs/ubuntu-hex/packages/install-virtualbox.sh
+++ configs/ubuntu-hex/packages/install-virtualbox.sh
@@ -9,12 +9,12 @@ sudo apt-get install -y \
     gnupg-agent \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://www.virtualbox.org/download/oracle_vbox.asc | sudo apt-key add -
+curl -fsSL https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo apt-key add -
 
 echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
-echo "# deb-src http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+echo "# deb-src http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | sudo tee -a /etc/apt/sources.list.d/virtualbox.list
 
-# sudo apt-get -y update
-# sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-# sudo docker run hello-world
-# sudo usermod -aG docker "$USER"
+sudo apt-get -y update
+sudo apt-cache search virtualbox | grep ^virtualbox
+sudo apt-get install -y virtualbox-6.1 dkms
