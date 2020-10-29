0e46680ccddd6057fd6016ee04a79d899875c7dd
murer <murer@dextra-sw.com>
2020-10-29 12:00:26 -0300

    x

--- poc/openerssh/docker.sh
+++ poc/openerssh/docker.sh
@@ -28,7 +28,6 @@ docker_devx() {
     -v "$(pwd)":/home/hexblade/openerssh \
     -e "DISPLAY=unix$DISPLAY" \
     -v "/tmp/.X11-unix:/tmp/.X11-unix" \
-    -v "$HOME/.ssh:/home/hexblade/.ssh" \
     murer/openerssh-dev:local "$@"
 }
 
