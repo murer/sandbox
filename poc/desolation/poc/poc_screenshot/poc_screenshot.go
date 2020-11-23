22ee36285ba0eb3bad7220e8bf3527bbfcbf8123
murer <murer@dextra-sw.com>
2020-11-23 12:49:49 -0300

    x

---
+++ poc/desolation/poc/poc_screenshot/poc_screenshot.go
@@ -0,0 +1,4 @@
+package poc_screenshot
+
+func noop() {
+}
---
+++ poc/desolation/poc/poc_screenshot/poc_screenshot_test.go
@@ -0,0 +1,11 @@
+package poc_screenshot_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func noop(t *testing.T) {
+	assert.Equal(t, 1, 1)
+}
