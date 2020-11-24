e3279954731043b132d0dd1342e60a43b9bbe7c4
murer <murer@dextra-sw.com>
2020-11-24 17:21:27 -0300

    x

--- poc/desolation/guest/guestserver.go
+++ poc/desolation/guest/guestserver.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/murer/desolation/message"
 	"github.com/murer/desolation/util"
 )
 
@@ -19,9 +20,35 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Access: %s %s %s", r.RemoteAddr, r.Method, r.URL)
 	if r.Method == "GET" && r.URL.Path == "/api/version.txt" {
 		util.RespText(w, util.Version)
+	} else if r.Method == "POST" && r.URL.Path == "/api/command" {
+		HandleCommand(w, r)
 	} else if strings.HasPrefix(r.URL.Path, "/self/") {
 	} else if r.Method == "POST" {
 	} else {
 		http.NotFound(w, r)
 	}
+}
+
+func HandleCommand(w http.ResponseWriter, r *http.Request) {
+	contentType := r.Header.Get("Content-Type")
+	reqBody := ""
+	if strings.HasPrefix(contentType, "application/json") {
+		reqBody = util.ReadAllString(r.Body)
+	} else {
+		r.ParseForm()
+		reqBody = r.Form.Get("msg")
+	}
+	if reqBody == "" {
+		log.Fatalf("Message not found: %s", contentType)
+	}
+	msg := message.Decode(reqBody)
+	var ret *message.Message
+	if msg.Name == "echo" {
+		ret = msg
+	} else {
+		ret = &message.Message{Name: "unknown", Headers: nil, Payload: ""}
+	}
+	respBody := message.Encode(ret)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(respBody))
 }
--- poc/desolation/guest/guestserver_test.go
+++ poc/desolation/guest/guestserver_test.go
@@ -1,11 +1,14 @@
 package guest_test
 
 import (
+	"bytes"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	"github.com/murer/desolation/guest"
+	"github.com/murer/desolation/message"
 	"github.com/murer/desolation/util"
 	"github.com/stretchr/testify/assert"
 )
@@ -22,14 +25,55 @@ func TestVersion(t *testing.T) {
 	assert.Equal(t, util.Version, util.ReadAllString(resp.Body))
 }
 
-func TestWrite(t *testing.T) {
+func TestUnknown(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(guest.Handle))
 	defer server.Close()
 	t.Logf("URL: %s", server.URL)
 
-	// msg := &message.Message{
-	// 	Name:    "echo",
-	// 	Headers: map[string]string{"foo": "1", "bar": "2"},
-	// 	Payload: "bXVyZXI=",
-	// }
+	msg := &message.Message{
+		Name:    "hfewi",
+		Headers: map[string]string{"foo": "1", "bar": "2"},
+		Payload: "bXVyZXI=",
+	}
+	resp, err := http.Post(server.URL+"/api/command", "application/json", bytes.NewReader([]byte(message.Encode(msg))))
+	util.Check(err)
+	assert.Equal(t, 200, resp.StatusCode)
+	assert.Equal(t, "application/json", resp.Header.Get("Content-Type"))
+	assert.Equal(t, &message.Message{Name: "unknown", Headers: nil, Payload: ""}, message.Decode(util.ReadAllString(resp.Body)))
+}
+
+func TestEchoJson(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(guest.Handle))
+	defer server.Close()
+	t.Logf("URL: %s", server.URL)
+
+	msg := &message.Message{
+		Name:    "echo",
+		Headers: map[string]string{"foo": "1", "bar": "2"},
+		Payload: "bXVyZXI=",
+	}
+	resp, err := http.Post(server.URL+"/api/command", "application/json", bytes.NewReader([]byte(message.Encode(msg))))
+	util.Check(err)
+	assert.Equal(t, 200, resp.StatusCode)
+	assert.Equal(t, "application/json", resp.Header.Get("Content-Type"))
+	assert.Equal(t, msg, message.Decode(util.ReadAllString(resp.Body)))
+}
+
+func TestEchoForm(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(guest.Handle))
+	defer server.Close()
+	t.Logf("URL: %s", server.URL)
+
+	msg := &message.Message{
+		Name:    "echo",
+		Headers: map[string]string{"foo": "1", "bar": "2"},
+		Payload: "bXVyZXI=",
+	}
+	resp, err := http.PostForm(server.URL+"/api/command", url.Values{
+		"msg": {message.Encode(msg)},
+	})
+	util.Check(err)
+	assert.Equal(t, 200, resp.StatusCode)
+	assert.Equal(t, "application/json", resp.Header.Get("Content-Type"))
+	assert.Equal(t, msg, message.Decode(util.ReadAllString(resp.Body)))
 }
