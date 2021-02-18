15d3ac8d296d06022b2e89c9003cfaa7a7cb2e03
murer <murer@dextra-sw.com>
2021-02-18 16:31:04 -0300

    x

--- poc/firebase/imp.js
+++ poc/firebase/imp.js
@@ -1,7 +1,7 @@
 var myelement__ = document.createElement('script');
 
 myelement__.setAttribute('type', 'module')
-myelement__.setAttribute('src','https://raw.githubusercontent.com/murer/sandbox/master/poc/firebase/teste.jsi');
+myelement__.setAttribute('src','https://raw.githubusercontent.com/murer/sandbox/master/poc/firebase/teste.js');
 
 document.head.appendChild(myelement__);
 
