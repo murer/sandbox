d297646521117105db09af72da79fcd7e1b1bbe5
murer <murer@dextra-sw.com>
2021-05-06 11:51:49 -0300

    x

--- proj/jsdices/jsdices.test.js
+++ proj/jsdices/jsdices.test.js
@@ -6,15 +6,11 @@ test('rollDice', () => {
   expect(jsdices.rollDice(1)).toBeLessThan(2)
 });
 
-test('poc', () => {
-
+test('basics', () => {
   let roll = jsdices.parse('2d6+?+4+?-${a}+${bb}+3', { a: 5, bb: '2d12' }, '1d8', '9')
-
   expect(roll.toString()).toBe("2d6+1d8+4+9-5+2d12+3")
   expect("a: " + roll).toBe("a: 2d6+1d8+4+9-5+2d12+3")
-
   expect(roll.avg()).toBe(35.5)
-
   expect(roll.rnd()).toBeGreaterThan(0)
 });
 
