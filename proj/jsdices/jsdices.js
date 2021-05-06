f1dc9fa81bafd27152ffaf2490b3f0bcfea73b9e
murer <murer@dextra-sw.com>
2021-05-06 11:10:13 -0300

    x

--- proj/jsdices/jsdices.js
+++ proj/jsdices/jsdices.js
@@ -18,10 +18,33 @@ class JSDicesRoll {
 
 }
 
+const parseNameds = (template, nameds) => {
+  var myregexp = /\$\{([A-Za-z0-9_]+)\}/g;
+  var match = myregexp.exec(template);
+  var ret = ""
+  var offset = 0
+  while (match != null) {
+    if (match.index > offset) {
+      ret += template.substring(offset, match.index)
+    }
+    let name = match[1]
+    let value = nameds[name]
+    ret += value
+    offset = match.index + match[0].length
+    console.log('aaa: ', template, name, value, match.index, template.substr(match.index, match[0].length), ret)
+    match = myregexp.exec(template);
+  }
+  if (offset < template.length) {
+    ret += template.substring(offset, template.length)
+  }
+  return ret
+}
+
 class JSDices {
 
   parse(template, nameds, ...binds) {
     template = template.toString()
+    template = parseNameds(template, nameds)
     return new JSDicesRoll(template)
   }
 
--- proj/jsdices/jsdices.test.js
+++ proj/jsdices/jsdices.test.js
@@ -2,7 +2,7 @@ const jsdices = require('./jsdices.js');
 
 test('poc', () => {
 
-  let roll = jsdices.parse('2d6+?+4+?-$a+$b', { a: 5, b: '2d12' }, '1d8', '9')
+  let roll = jsdices.parse('2d6+?+4+?-${a}+${bb}+3', { a: 5, bb: '2d12' }, '1d8', '9')
 
   expect(roll.toString()).toBe("aaa")
   expect("a: " + roll).toBe("a: aaa")
