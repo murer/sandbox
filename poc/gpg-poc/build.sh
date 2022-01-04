7f1889deaa71dedff97f19e8ba3c0a9212dfddd0
murer <murer@dextra-sw.com>
2022-01-04 18:52:06 -0300

    x

--- poc/gpg-poc/build.sh
+++ poc/gpg-poc/build.sh
@@ -12,9 +12,50 @@ cmd_data() {
     echo c > target/files/x/c.txt
 }
 
+cmd_gpg_create_key() {
+    mkdir -p target/gpgkeys
+    gpg --homedir target/gpgkeys --batch --generate-key keydata.txt
+    #gpg --homedir target/gpgkeys --batch --generate-key --passphrase '' pyrata rsa
+}
+
+cmd_gpg_list_key() {
+    gpg --home target/gpgkeys --list-keys
+    gpg --home target/gpgkeys --list-public-keys
+    gpg --home target/gpgkeys --list-secret-keys
+}
+
+cmd_gpg_export_key() {
+    rm -rf target/keys || true
+    mkdir -p target/keys
+    gpg --home target/gpgkeys --export-secret-keys --armor pyrata > target/keys/pyrata.private.key
+    gpg --home target/gpgkeys --export --armor pyrata > target/keys/pyrata.public.key
+}
+
+cmd_gpg_reimport_public_key() {
+    rm -rf target/gpgkeys
+    mkdir -p target/gpgkeys
+    gpg --home target/gpgkeys --import target/keys/pyrata.public.key
+}
+
+cmd_gpg_reimport_private_key() {
+    rm -rf target/gpgkeys
+    mkdir -p target/gpgkeys
+    gpg --home target/gpgkeys --import target/keys/pyrata.private.key
+}
+
 cmd_test() {
-    cmd_clean
-    cmd_data
+    # cmd_clean
+    # cmd_data
+    # cmd_gpg_create_key
+    # cmd_gpg_list_key
+    # cmd_gpg_export_key
+
+    # cmd_gpg_reimport_public_key
+    # cmd_gpg_list_key
+
+    # cmd_gpg_reimport_private_key
+    # cmd_gpg_list_key
+    
     echo SUCCESS
 }
 
