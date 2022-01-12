12324c63ce19d413f4541626e79c7e05e0bea923
murer <murer@dextra-sw.com>
2022-01-12 16:48:56 -0300

    x

--- proj/tesourodireto/build.sh
+++ proj/tesourodireto/build.sh
@@ -10,18 +10,20 @@ cmd_clean() {
 }
 
 cmd_run() {
-  docker run -it --rm --label tesourodireto_dev \
+  docker run -it --rm --label tesourodireto_dev --name tesourodireto_dev \
     -v "$(pwd)/src:/home/hexblade/tesourodireto" \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -p 5900:5900 -p 127.0.0.1:8888:8888 \
     tesourodireto/tesourodireto:dev "$@"
 }
 
 cmd_run_own() {
-  docker run -it --rm --label tesourodireto_dev \
+  docker run -it --rm --label tesourodireto_dev --name tesourodireto_dev \
     -v "$(pwd)/src:/home/hexblade/tesourodireto" \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e "DISPLAY=:0" \
-    tesourodireto/tesourodireto:dev python3 tesourodireto/sample_test.py
+    --p 127.0.0.1:8888:8888 \
+    tesourodireto/tesourodireto:dev # python3 tesourodireto/sample_test.py
 }
 
 cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"
