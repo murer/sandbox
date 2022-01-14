0793ac2c1669abd7e35cba82b5749acd3ab4f874
murer <fuweweu@gmail.com>
2022-01-13 21:29:34 -0300

    x

--- proj/tesourodireto/src/tesouroexprt.js
+++ proj/tesourodireto/src/tesouroexprt.js
@@ -6,7 +6,6 @@
             url: '/Protocolo/' + protocolo + '/1', 
             dataType: 'html', 
             success: function(result) {
-                console.log('result', result)
                 var r = $(result)
                 var ret = {}
                 ret.titulo = r.find('.td-protocolo-info-titulo').text().trim()
@@ -18,6 +17,7 @@
                 ret.taxaInst = r.find('.td-protocolo-info:contains("Taxa da inst") .td-protocolo-info--valor').text().trim()
                 ret.dia = r.find('.td-protocolo-info-base:contains("Data da op") .td-protocolo-info-base--blue').text().trim()
                 ret.inst = r.find('.td-protocolo-info-base:contains("Institui") .td-protocolo-info-base--blue').text().trim()
+                ret.valorBruto = r.find('.td-protocolo-info:contains("Valor bruto"):first .td-protocolo-info--valor').text().trim()
                 cb(ret)
             }
         })
@@ -34,13 +34,14 @@
             '<th>inst</th>' + 
             '<th>taxaInst</th>' + 
             '<th>taxaB3</th>' + 
+            '<th>valorLiquido</th>' + 
+            '<th>valorBruto</th>' + 
             '</tr></thread></table></body></html>')
         $('table tbody', window.sandboxPopup.document).remove()
         $('table', window.sandboxPopup.document).append('<tbody/>')
         var tbody = $('table tbody', window.sandboxPopup.document)
         prots.forEach(element => {
             var tr = tbody.append('<tr/>').children('tr:last')
-            console.log('aaaa', element, tr.length)
             tr.append('<td/>').find('td:last').text(element.dia)
             tr.append('<td/>').find('td:last').text(element.titulo.toLowerCase().replaceAll(' ', '-'))
             tr.append('<td/>').find('td:last').text(element.qtd)
@@ -48,17 +49,38 @@
             tr.append('<td/>').find('td:last').text(element.inst)
             tr.append('<td/>').find('td:last').text(element.taxaInst.replaceAll('R$', '').replaceAll('.', ''))
             tr.append('<td/>').find('td:last').text(element.taxaB3.replaceAll('R$', '').replaceAll('.', ''))
+            tr.append('<td/>').find('td:last').text(element.valorLiquido.replaceAll('R$', '').replaceAll('.', ''))
+            tr.append('<td/>').find('td:last').text(element.valorBruto.replaceAll('R$', '').replaceAll('.', ''))
         });
     }
 
-    var protocolos = $('a[href^="/Protocolo/"]').map((a, b) => $(b).attr('href').split('/')[2]).toArray();
+    var protocoloIds = $('a[href^="/Protocolo/"]').map((a, b) => $(b).attr('href').split('/')[2]).toArray();
+    var protocolos = []
+
+    function nextProtocol() {
+        if(protocoloIds.length == 0) {
+            console.log('redering', protocolos)
+            showResults(protocolos)
+            console.log('done')
+            return
+        }
+        loadProtocolo(protocoloIds.shift(), function(protocolo) {
+            console.log(protocolo)
+            protocolos.push(protocolo)
+            nextProtocol()
+            return
+        })
+    }
+
+    nextProtocol()
+
     // protocolos.forEach(p => {
     //     console.log('protocolo', p)
     // })
     
-    loadProtocolo('20905065', function(protocolo) {
-        console.log(protocolo)
-        showResults([protocolo])
-    })
+    //loadProtocolo('20905065', function(protocolo) {
+    //    console.log(protocolo)
+    //    showResults([protocolo])
+    //})
 
 })(jQuery)
