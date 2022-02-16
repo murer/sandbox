3e18b628d77b8a47deca6fe6bd139661e933320c
murer <murer@ciandt.com>
2022-02-16 15:31:16 -0300

    x

---
+++ poc/couchdb/cauchdb/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -xe
+
+function cmd_clean() {
+  docker ps -aq --filter label=couchdb-poc | xargs docker rm -f || true
+  docker system prune --volumes --filter label=couchdb-poc -f || true
+}
+
+
+function cmd_run() {
+  docker volume create --label couchdb-poc couchdb-poc
+	docker --rm -it --label couchdb-poc --name couchdb-poc \
+    --mount 'src=couchdb-poc,dst=/opt/couchdb/data'
+	  apache/couchdb:3	
+}
+
+set +x; cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; set -x; "cmd_${_cmd}" "$@"
