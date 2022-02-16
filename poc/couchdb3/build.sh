f9c10f1a180350f612695fafe23076f069213c00
murer <murer@ciandt.com>
2022-02-16 16:33:15 -0300

    x

--- poc/couchdb3/build.sh
+++ poc/couchdb3/build.sh
@@ -15,4 +15,22 @@ function cmd_run() {
 	  apache/couchdb:3	
 }
 
+function cmd_view_update_map() {
+  curl -vX PUT http://admin:123@127.0.0.1:5984/mydb/_design/my_test \
+     -d '{"views":{"my_filter":{"map":
+         "function(doc) { if(doc.name) { emit(doc.name, doc); }}"}}}'
+}
+
+function cmd_view_update_reduce() {
+  curl -vX PUT http://admin:123@127.0.0.1:5984/mydb/_design/my_reduce \
+     -d '{
+        "views": { 
+          "my_filter": {
+            "map": "function(doc) { if(doc.name) { emit(doc.name, doc); }}",
+            "reduce": "function() { return 1; }"
+          }
+        }
+      }'
+}
+
 set +x; cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; set -x; "cmd_${_cmd}" "$@"
