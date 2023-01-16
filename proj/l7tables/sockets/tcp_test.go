17430e00738680e76af4ded6b84a71a03c51566a
murer <murer@ciandt.com>
2023-01-16 17:46:32 -0300

    x

---
+++ proj/l7tables/sockets/tcp_test.go
@@ -0,0 +1,22 @@
+package sockets
+
+import (
+	"io"
+	"net"
+	"testing"
+
+	"github.com/murer/l7tables/util"
+)
+
+func TestListen(t *testing.T) {
+
+	server, err := net.Listen("tcp4", "127.0.0.1:5000")
+	util.Check(err)
+	defer server.Close()
+	conn, err := server.Accept()
+	util.Check(err)
+	defer conn.Close()
+	io.ReadAll(conn)
+
+	// assert.Equal(t, "OK", Ok())
+}
--- proj/l7tables/util/util.go
+++ proj/l7tables/util/util.go
@@ -1,5 +1,11 @@
 package util
 
+func Check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func Ok() string {
 	return "OK"
 }
