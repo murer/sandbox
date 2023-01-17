ab2c74baa8b301007ce3617ea1627c66b8c6951a
murer <murer@ciandt.com>
2023-01-17 17:37:44 -0300

    x

--- proj/l7tables/main.go
+++ proj/l7tables/main.go
@@ -1,11 +1,9 @@
 package main
 
-import "github.com/murer/l7tables/listens"
-
 func main() {
-	listens := listens.Create()
-	defer listens.Close()
-	listens.Listen("tcp4", "127.0.0.1:5005")
-	listens.Listen("tcp4", "127.0.0.1:5006")
-	listens.Accepts()
+	// listens := listens.Create()
+	// defer listens.Close()
+	// listens.Listen("tcp4", "127.0.0.1:5005")
+	// listens.Listen("tcp4", "127.0.0.1:5006")
+	// listens.Accepts()
 }
