db4eb777760ac9c5dc7803bc61b53292c31d5042
murer <murer@ciandt.com>
2023-01-17 18:26:30 -0300

    x

--- proj/l7tables/main.go
+++ proj/l7tables/main.go
@@ -1,9 +1,24 @@
 package main
 
 func main() {
-	// listens := listens.Create()
-	// defer listens.Close()
-	// listens.Listen("tcp4", "127.0.0.1:5005")
-	// listens.Listen("tcp4", "127.0.0.1:5006")
-	// listens.Accepts()
+
+	a := func() {
+
+	}
+	a()
+
+	// kernel.Init()
+
+	// tcp = kernel.addModule(TcpListenerModule())
+
+	// kernel.addModule(ForwardTCPModule())
+	// 		.rule(func(conn Conn) {
+	//          if .... return "a.a.a.a:5010"
+	//		 })
+
+	// defer kernel.Close()
+
+	// tcp.listen("tcp4", "127.0.0.1:5005")
+
+	// kernel.Wait()
 }
