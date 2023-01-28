dc9b905566eae459f6e925065bec87922aead709
murer <murer@ciandt.com>
2023-01-28 18:41:04 -0300

    x

--- proj/roll21/src/roll21-03-table.js
+++ proj/roll21/src/roll21-03-table.js
@@ -4,22 +4,31 @@
 
     $d.find('#wrapper').html('')
 
-    function addRow(id, req, resp) {
+    function addRow(id, rid, req, resp) {
         var rt = $d.find('#' + id)
         if(!rt.length) {
             console.log('aa', $d.find('#wrapper'))
-            $d.find('#wrapper').append('<div id="' + id + '"><h3>' + id + '</h3><table><thead><tr><th>roll</th><th>action</th></tr></thead></table></div>')
-            $d.find('table').attr('border', '1')
+            $d.find('#wrapper').append('<div id="' + id + '"><h3>' + id + '</h3><table><thead><tr><th>roll</th><th>action</th></tr></thead><tbody></tbody></table></div>')
+            $d.find('table').attr('border', '1').attr('cellpadding', '10')
             rt = $d.find('#' + id)
             req.rolls.forEach(v => {
                 console.log('v', v)
                 rt.find('thead tr').append('<th/>').find('th:last').append(v.rollid) 
             })           
         }
-        
+        var tr = rt.find('tbody').append('<tr><td class="rollid"/><td class="action"/></tr>').find('tr:last')
+        tr.find('td.rollid').text(rid)
+        req.rolls.forEach(v => {
+            tr.append('<td/>').find('td:last').addClass('roll21' + v.rollid).text(v.rollid)
+        })
+        for (var k in resp) {
+            var rolldata = JSON.parse(resp[k].json)
+            console.log('k', k, rolldata)
+            tr.find('td.roll21' + k).text('' + rolldata.total)
+        }
     }
 
-    addRow('aa', {
+    addRow('aa', 'bb', {
     "cid": "campaign-11896033-axRZK6G08g4r9r-Czt5xHA",
     "fbnum": "https://roll20-99960.firebaseio.com/",
     "authkey": "eyJhbGciOiJSUzI1NiIsImtpZCI6ImQwNWI0MDljNmYyMmM0MDNlMWY5MWY5ODY3YWM0OTJhOTA2MTk1NTgiLCJ0eXAiOiJKV1QifQ.eyJjdXJyZW50Y2FtcGFpZ24iOiJjYW1wYWlnbi0xMTg5NjAzMy1heFJaSzZHMDhnNHI5ci1DenQ1eEhBIiwiaXNfZ20iOmZhbHNlLCJwbGF5ZXJpZCI6Ii1NbzFMbjZJazA0WnpPNVZ4a01XIiwidXNlcmlkIjo5ODIyNDMsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS9yb2xsMjAtZGV2IiwiYXVkIjoicm9sbDIwLWRldiIsImF1dGhfdGltZSI6MTY3NDk0MDI3NiwidXNlcl9pZCI6Ijk4MjI0MyIsInN1YiI6Ijk4MjI0MyIsImlhdCI6MTY3NDk0MDI3NiwiZXhwIjoxNjc0OTQzODc2LCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7fSwic2lnbl9pbl9wcm92aWRlciI6ImN1c3RvbSJ9fQ.JpEJ_4SC4on-pl4MRU0HmQyg9kZ_n1gG-PXnpg5KrngY1VEx8Ncl-EHSZdJ2e3bt0qIoWBkASi6qvz2mkKc4Mxj0-cMN_-65p6CNM4buRxVjYoSr-SXKG6_sbENiopLS25fFlXBDiAiJXtGVa1AvhW--y-_-fErPpUDm_UxNHGujDn13kHKHI3QGaQeruepRU1J5XpoFW6Ln6p1PFggCrCf7b7V9sHXo-Lr2Zv_0Zu6N3taS81KflTk-vuItuPWLiP6nP_V0vcB1tfP9dSh7l08FZ1lKqjwPD71FAkxb5tPSzg7MmI2TSP3I60VyUvieFeKGKYvI3P9F3uj6EppN5A",
